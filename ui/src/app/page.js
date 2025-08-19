ce29a2f04000cd2561633236c5a34eec4f5fc8eb
- - - - - - <20849315+dresean@users.noreply.github.com>
2025-08-19 15:53:04 -0500

    update meta data and home page, install react and next

---
+++ ui/src/app/page.js
@@ -0,0 +1,9 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <div>
+      dresean.com is being built right now! stay tuned for updates :) 
+    </div>
+  );
+}
