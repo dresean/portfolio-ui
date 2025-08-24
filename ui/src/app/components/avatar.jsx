710abd3d88379ceceb064e529a725caa42ac69e7
- - - - - - <20849315+dresean@users.noreply.github.com>
2025-08-24 11:26:52 -0500

    cleanup & readme update

--- ui/src/app/components/avatar.jsx
+++
@@ -1,6 +0,0 @@
-// import picture from '../../../public/dreavi.jpg'
-export default function Avatar() {
-    return (
-        <img src="../../../../ui/public/dreavi.jpg" alt="Picture of Dre'Sean"></img>
-    )
-}
--- ui/src/app/components/practice.jsx
+++
@@ -1,8 +0,0 @@
-function click() {
-    console.log('The button was clicked')
-}
-export default function MyButton() {
-    return (
-        <button >Click me</button>
-    )
-}
