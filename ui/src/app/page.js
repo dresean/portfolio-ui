f76f0521eeeabc05808d7c1fe4ac294a9fbddf28
- - - - - - <20849315+dresean@users.noreply.github.com>
2025-08-24 10:43:54 -0500

    add responsiveness and breakpoints

--- ui/src/app/page.js
+++ ui/src/app/page.js
@@ -1,11 +1,26 @@
+
+"use client"
 // import Avatar from "./components/avatar";
 import dreavi from "../../public/dreavi.jpg"
 import MyButton from "./components/practice"
 import "./style/home.scss"
+
+import * as motion from "motion/react-client"
+
+const box = {
+    width: 100,
+    height: 100,
+    backgroundColor: "#305870",
+    borderRadius: 5,
+    marginLeft: "30px"
+}
 export default function Home() {
   return (
+    <>
     <div className="header">
       dresean.com is being built right now! stay tuned for updates :) 
     </div>
+    <motion.div style={box} animate={{ rotate: 5040 }} transition={{ duration: 120 }}/>
+    </>
   );
 }
