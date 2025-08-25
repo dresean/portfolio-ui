bac96ddd3c99cc73bcdb61cadbf123b6e2b025c1
- - - - - - <20849315+dresean@users.noreply.github.com>
2025-08-24 21:44:01 -0500

    add google fonts

--- ui/src/app/layout.js
+++ ui/src/app/layout.js
@@ -1,4 +1,4 @@
-import { Geist, Geist_Mono } from "next/font/google";
+import { Geist, Geist_Mono, Vibes, Fleur_De_Leah, Major_Mono_Display, Dorsa, Miniver, Anton_SC } from "next/font/google";
 import "./globals.css";
 
 const geistSans = Geist({
@@ -11,6 +11,36 @@ const geistMono = Geist_Mono({
   subsets: ["latin"],
 });
 
+const vibes = Vibes({
+ subsets: ['latin'],
+  weight: ['400']
+})
+
+const fleurDeLeah = Fleur_De_Leah({
+ subsets: ['latin'],
+  weight: ['400']
+})
+
+const majorMonoDisplay = Major_Mono_Display({
+ subsets: ['latin'],
+  weight: ['400']
+})
+
+const dorsa = Dorsa({
+ subsets: ['latin'],
+  weight: ['400']
+})
+
+const miniver = Miniver({
+ subsets: ['latin'],
+  weight: ['400']
+})
+
+const antonSC = Anton_SC({
+  subsets: ['latin'],
+  weight: ['400']
+})
+
 export const metadata = {
   title: "Dre'Sean",
   description: "Created with love with React.js",
@@ -20,7 +50,8 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        className={miniver.className}
+        // todo remove && add default font! oh the choices!
       >
         {children}
       </body>
